ee58f6eab3dd2becc02d061d4cc41a525a72e873
Matthew Peters <JiveTurkey@MattBook-Pro.local>
2018-06-05 09:46:31 -0600

    builds pantry that has stock and shopping_list that can add recipe ingredients

--- lib/pantry.rb
+++ lib/pantry.rb
@@ -1,8 +1,11 @@
+require 'pry'
+
 class Pantry
-  attr_reader :stock
+  attr_reader :stock, :shopping_list
 
   def initialize
     @stock = Hash.new(0)
+    @shopping_list = Hash.new(0)
   end
 
   def stock_check(item)
@@ -13,4 +16,10 @@ class Pantry
     @stock[item] += quantity
   end
 
+  def add_to_shopping_list(recipe)
+    items = recipe.ingredients
+    @shopping_list = @shopping_list.merge(items) do |item, list_qty, recipe_qty|
+      list_qty + recipe_qty
+    end
+  end
 end
--- test/pantry_test.rb
+++ test/pantry_test.rb
@@ -1,27 +1,33 @@
 require './lib/pantry'
+require './lib/recipe'
 require 'minitest/autorun'
 require 'minitest/pride'
+require 'pry'
 
 class PantryTest < Minitest::Test
+  def setup
+    @pantry = Pantry.new
+    @r = Recipe.new("Cheese Pizza")
+  end
 
   def test_has_stock_of_items
-    pantry = Pantry.new
-    assert_instance_of Hash, pantry.stock
-    assert_equal 0, pantry.stock.length
+    assert_instance_of Hash, @pantry.stock
+    assert_equal 0, @pantry.stock.length
   end
 
-  def test_patry_checks_its_stock_and_adds_to_stock
-    pantry = Pantry.new
-    assert_equal 0, pantry.stock_check("Cheese")
-    pantry.restock("Cheese", 10)
-    assert_equal 10, pantry.stock_check("Cheese")
+  def test_patry_checks_its_stock_and_adds_to_existing_stock
+    assert_equal 0, @pantry.stock_check("Cheese")
+    @pantry.restock("Cheese", 10)
+    assert_equal 10, @pantry.stock_check("Cheese")
+    @pantry.restock("Cheese", 20)
+    assert_equal 30, @pantry.stock_check("Cheese")
   end
 
-  def test_pantry_adds_to_existing_stock
-    pantry.restock("Cheese", 20)
-    pantry.stock_check("Cheese")
-    # => 30
+  def test_pantry_has_shopping_list_and_can_add_ingredients
+    @r.add_ingredient("Flour", 20)
+    @r.add_ingredient("Cheese", 20)
+    @pantry.add_to_shopping_list(@r)
+    expected = {"Cheese" => 20, "Flour" => 20}
+    assert_equal expected, @pantry.shopping_list
   end
-
-
 end
